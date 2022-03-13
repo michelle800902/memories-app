caab7adc786255d9f6ddbbcb123f014402c6bfa1
Michelle Chen <michelle800902@gmail.com>
2022-03-13 17:00:17 +0800

    Fix wrong import dependencies in server index

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 import express from 'express';
-import bodyParser from './bodyParser';
-import mongoose from './mongoose';
-import cors from './cors';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
 
 const app = express();
 app.use(bodyParser.json({ limit: '30mb', extended: true }));
@@ -14,8 +14,6 @@ const PORT = process.env.PORT || 5000;
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
     .catch((error) => console.log(error.message));
-
-mongoose.set('useFindAndModify', false);
 
 // DB: MongoDB
 // https://www.mongodb.com/cloud/atlas
