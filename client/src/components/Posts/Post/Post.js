c96169fec9304808a647e7a1d62b04eceba44382
Michelle Chen <michelle800902@gmail.com>
2022-03-21 01:08:01 +0800

    Update layout of like in component Post

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -47,8 +47,7 @@ const Post = ({ post, setCurrentId }) => {
             <CardActions className={classes.cardActions}>
                 <Button size="small" color="primary" onClick={() => dispatch(likePost(post._id))}>
                     <ThumbUpAltIcon fontSize="small" />
-                    Like
-                    {post.likeCount}
+                    {`Like ${post.likeCount}`}
                 </Button>
                 <Button size="small" color="primary" onClick={() => dispatch(deletePost(post._id))}>
                     <DeleteIcon fontSize="small" />
