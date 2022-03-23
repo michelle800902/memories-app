0711d4d577e091e9cf26994518bde9971adba652
Michelle Chen <michelle800902@gmail.com>
2022-03-23 18:52:53 +0800

    Update server and setup heroku env with Procfile

--- server/index.js
+++ server/index.js
@@ -5,22 +5,26 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import postRoutes from './routes/posts.js';
 
-const app = express();
-
 dotenv.config();
+
+const CONNECTION_URL = process.env.CONNECTION_URL;
+const PORT = process.env.PORT || 5000;
+
+const app = express();
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
 
 app.use('/posts', postRoutes);
+app.get('/', (req, res) => {
+    res.send('Memories API');
+});
 
-const CONNECTION_URL = process.env.CONNECTION_URL;
-const PORT = process.env.PORT || 5000;
-
+/**
+ * DB: MongoDB
+ * https://www.mongodb.com/cloud/atlas
+ */
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
     .catch((error) => console.log(error.message));
-
-// DB: MongoDB
-// https://www.mongodb.com/cloud/atlas
